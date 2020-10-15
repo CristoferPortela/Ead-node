e71e8ad217c281c0eb88b4271569e64a7102556a
Cristofer Portela <cristoferportela@gmail.com>
2020-10-15 18:51:19 -0300

    feat: caminho de rotas criado

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ require( 'dotenv-safe' ).config();
 const server = require('./server');
 require( './services/mongo.service' );
 
+const routes = require( './routes' );
+
 /**
  * Iniciando nossa aplicação, aqui é a função principal do hapi
  */
 const init = async () => {
 
-
-   server.route({
-      method: 'GET',
-      path: '/',
-      handler: () => 'Hello World!',
-   });
+   server.route( routes );
 
    await server.start();
    console.log('Server running on %s', server.info.uri);
